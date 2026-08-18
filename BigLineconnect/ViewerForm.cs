43e44837f8b6e7968b51938b6517d3c975fa31db
tumham <my@bigus.com.tr>
2026-08-18 22:57:11 +0300

    Official FrameRelayPump implementation with Channel Bounded(1) DropOldest for zero backlog (v3.34.0)

--- BigLineconnect/ViewerForm.cs
+++ BigLineconnect/ViewerForm.cs
@@ -251,7 +251,7 @@ namespace BigLineconnect
         }
         private void InitializeComponent()
         {
-            this.Text = LanguageManager.Get("title_viewer", _targetId) + " - v3.33.0 (Zero-Backlog Non-Blocking Relay Engine)";
+            this.Text = LanguageManager.Get("title_viewer", _targetId) + " - v3.34.0 (FrameRelayPump Zero-Backlog Engine)";
             this.Size = new Size(1280, 768);
             this.StartPosition = FormStartPosition.CenterScreen;
             this.BackColor = Color.Black;
