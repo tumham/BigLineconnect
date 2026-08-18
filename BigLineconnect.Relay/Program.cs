370fb37f9a3551ee886c85d7b4112492f2d2cf69
tumham <my@bigus.com.tr>
2026-08-19 00:38:09 +0300

    Set Relay WebSocket KeepAliveInterval to 3s for zero-idle latency

--- BigLineconnect.Relay/Program.cs
+++ BigLineconnect.Relay/Program.cs
@@ -621,7 +621,7 @@ namespace BigLineconnect.Relay
             
             app.UseWebSockets(new WebSocketOptions
             {
-                KeepAliveInterval = TimeSpan.FromSeconds(30)
+                KeepAliveInterval = TimeSpan.FromSeconds(3)
             });
 
             app.MapGet("/download", async context =>
