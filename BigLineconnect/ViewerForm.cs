019721e6805a8ccee624de00ed6438de9eb1caf2
tumham <my@bigus.com.tr>
2026-08-22 21:55:29 +0300

    v3.63.8: Add UDP P2P Probe Engine for 50ms Direct Transport with 100% Zero-Risk Fallback Guarantee to TCP Relay

--- BigLineconnect/ViewerForm.cs
+++ BigLineconnect/ViewerForm.cs
@@ -273,7 +273,7 @@ namespace BigLineconnect
         }
         private void InitializeComponent()
         {
-            this.Text = LanguageManager.Get("title_viewer", _targetId) + " - v3.63.7 (Commercial PRO License & 10-Minute Free Session Limits Engine)";
+            this.Text = LanguageManager.Get("title_viewer", _targetId) + " - v3.63.8 (Commercial PRO License & 10-Minute Free Session Limits Engine)";
             this.Size = new Size(1280, 768);
             this.StartPosition = FormStartPosition.CenterScreen;
             this.BackColor = Color.Black;
