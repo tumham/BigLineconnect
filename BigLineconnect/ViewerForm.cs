1cdf27d78d4bb1181919c6a9b421f6645345dff7
tumham <my@bigus.com.tr>
2026-08-24 19:17:20 +0300

    v3.69.0: Add brand-new ultra-simple & effective BigLineTransfer v3.0 copy-paste and drag-drop file transfer model

--- BigLineconnect/ViewerForm.cs
+++ BigLineconnect/ViewerForm.cs
@@ -376,7 +376,7 @@ namespace BigLineconnect
         }
         private void InitializeComponent()
         {
-            this.Text = LanguageManager.Get("title_viewer", _targetId) + " - v3.68.0 (Commercial PRO License & 10-Minute Free Session Limits Engine)";
+            this.Text = LanguageManager.Get("title_viewer", _targetId) + " - v3.69.0 (Commercial PRO License & 10-Minute Free Session Limits Engine)";
             this.Size = new Size(1280, 768);
             this.StartPosition = FormStartPosition.CenterScreen;
             this.BackColor = Color.Black;
